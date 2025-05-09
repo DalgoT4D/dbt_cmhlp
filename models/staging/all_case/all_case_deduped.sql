2d4ae3cb00e823a9dbe819a5927675b5d219b3e3
Ishankoradia <ikoradia@umich.edu>
2025-05-09 15:13:53 +0530

    update test cases

--- models/staging/all_case/all_case_deduped.sql
+++ models/staging/all_case/all_case_deduped.sql
@@ -1,10 +1,11 @@
 -- Deduplicate the all_case_meta_data table. 
--- For each district & case_type, we should take the latest record of case_id
+-- We should take the latest record of case_id
+-- A case in commcare can span across multiple apps (districts)
 
 {{ 
     dbt_utils.deduplicate(
         relation=ref('all_case_meta_data'),
-        partition_by='district_name, case_type, case_id',
+        partition_by='case_id',
         order_by="indexed_on DESC"
     )
 }}
